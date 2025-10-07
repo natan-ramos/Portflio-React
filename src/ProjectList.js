46b370d26e495d077d628737f81209fbad9d6e77
Natan Ramos <natanbarbara90@gmail.com>
2025-10-07 12:59:43 -0300

    feat: Adiciona Pomodoro e remove projeto To-do da lista

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -6,37 +6,39 @@ import './ProjectList.css';
 // 1. As imagens são importadas da pasta 'src/images/'
 import portfolioImage from './images/portfolio-react.png';
 import todoAppImage from './images/todo-app.png';
+import pomodoroImage from './images/pomodoro-timer.png'; // <-- NOVA IMPORTAÇÃO
 
 // 2. O objeto 'images' usa a string do JSON como chave
 const images = {
-  // ATENÇÃO: Essas chaves devem ser iguais à string "image" do seu projects.json
-  '/images/portfolio-react.png': portfolioImage,
-  '/images/todo-app.png': todoAppImage
+  // ATENÇÃO: Essas chaves devem ser iguais à string "image" do seu projects.json
+  '/images/portfolio-react.png': portfolioImage,
+  '/images/todo-app.png': todoAppImage,
+  '/images/pomodoro-timer.png': pomodoroImage // <-- NOVO MAPEAMENTO
 };
 
 function ProjectList() {
-  return (
-    <section>
-      <h2>Meus Projetos</h2>
-      <div className="project-list-container">
-        {projects.map(project => (
-          <Link to={`/projetos/${project.id}`} key={project.id} className="project-card-link">
-            <div className="project-card">
-              <img
-                src={images[project.image]} // Aqui a chave do JSON é usada para buscar a imagem
-                alt={project.title}
-                className={`project-image ${project.id === 1 || project.id === 2 ? 'grayscale-faded zoomed-image' : ''} ${project.id === 1 ? 'contrast-enhanced' : ''}`}
-              />
-              <div className="project-content">
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-              </div>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </section>
-  );
+  return (
+    <section>
+      <h2>Meus Projetos</h2>
+      <div className="project-list-container">
+        {projects.map(project => (
+          <Link to={`/projetos/${project.id}`} key={project.id} className="project-card-link">
+            <div className="project-card">
+              <img
+                src={images[project.image]} // Aqui a chave do JSON é usada para buscar a imagem
+                alt={project.title}
+                className={`project-image ${project.id === 1 || project.id === 2 ? 'grayscale-faded zoomed-image' : ''} ${project.id === 1 ? 'contrast-enhanced' : ''}`}
+              />
+              <div className="project-content">
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default ProjectList;
